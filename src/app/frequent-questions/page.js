8d7b61e68dcfc4a54d6797f4e9b35504c53d6bdc
OzkrMebasser <oscarmorenoestudios@gmail.com>
2023-07-03 23:45:26 -0700

    fix: scrollButton frequent questions

--- src/app/frequent-questions/page.js
+++ src/app/frequent-questions/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useRef } from "react";
-import ScrollToTop from "../components/Scroll/ScrolltoTop";
-import { MdArrowDropDownCircle } from "react-icons/md";
+// import ScrollToTop from "../components/Scroll/ScrolltoTop";
+// import { MdArrowDropDownCircle } from "react-icons/md";
 import FrequentQuestionOne from "./how-do-i-acquire-a-property-in-mexico/FrequentQuestionOne";
 import FrequentQuestionTwo from "./can-i-acquire-in-co-ownership/FrequentQuestionTwo";
 
@@ -10,20 +10,10 @@ const FrequentQuestions = (props) => {
 
   return (
     <>
-      <div class=" mt-[6rem] md:mb-0 w-full max-w-screen-md mx-auto relative">
-        <div class="flex justify-around pl-4 pr-4 text-xl text-[#9c8966] ">
-          {/* <div onClick={() => scrollDown(q1)}> Question 1</div>
-          <div onClick={() => scrollDown(q1)}> Question 2</div>
-          <div onClick={() => scrollDown(q1)}> Question 3</div> */}
-    <button>Ir a la sección 1</button>
-      <button>Ir a la sección 2</button>
-      <button>Ir a la sección 3</button>
 
-        </div>
-      </div>
       <div class="max-w-screen-xl mx-auto divide-y divide-width">
         <div className=""></div>
-        <ScrollToTop />
+        {/* <ScrollToTop /> */}
         <FrequentQuestionOne />
         <FrequentQuestionTwo />
         <FrequentQuestionOne />
