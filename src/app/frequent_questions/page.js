47a3b7e141887b373a8d802ee097c8dfd7252aa1
OzkrMebasser <oscarmorenoestudios@gmail.com>
2023-07-05 00:04:10 -0700

    new errors fixing

--- src/app/frequent_questions/page.js
+++ src/app/frequent_questions/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 
-import FrequentQuestionOne from "./how-do-i-acquire-a-property-in-mexico/FrequentQuestionOne";
-import FrequentQuestionTwo from "./can-i-acquire-in-co-ownership/FrequentQuestionTwo";
+import FrequentQuestionOne from "./How_do_I_acquire_a_property_in_Mexico/FrequentQuestionOne";
+import FrequentQuestionTwo from "./Can_I_acquire_in_co_ownership/FrequentQuestionTwo";
 
 const FrequentQuestions = (props) => {
 
